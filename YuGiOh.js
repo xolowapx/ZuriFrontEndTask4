4b58c30af283aa6bc1a952ab1411164037f07cc0
xolowapx <53216282+xolowapx@users.noreply.github.com>
2021-05-17 15:22:50 +0100

    Add files via upload

---
+++ YuGiOh.js
@@ -0,0 +1,44 @@
+/*
+Program: Zuri I4G Internship Training
+Exercise: FrontEnd Task 4B
+Intern Name: Solomon Ibeh;
+Slack Username: Solomon;
+Repo URL: 
+*/
+
+function checkYuGiOh(n) {
+    let multiples = [];
+
+    for(let i=1; i<=n ; i++) {
+        multiples.push(i)
+
+         if (i % 2 == 0 && i % 3 !== 0 && i % 5 !== 0) {
+          multiples.push('yu')
+        }
+        else if (i % 3 == 0 && i % 2 !== 0 && i % 5 !== 0) {
+          multiples.push('gi')
+        }
+        else if (i % 5 == 0 && i % 3 !== 0 && i % 2 !== 0) {
+          multiples.push('oh')
+        }
+        else if (i % 5 == 0 && i % 3 == 0 && i % 2 !== 0) {
+          multiples.push('gi-oh')
+        }
+        else if (i % 5 == 0 && i % 3 !== 0 && i % 2 == 0) {
+          multiples.push('yu-oh')
+        }
+        else if (i % 5 !== 0 && i % 3 == 0 && i % 2 == 0) {
+          multiples.push('yu-gi')
+        }
+        else if (i % 5 == 0 && i % 3 == 0 && i % 2 == 0) {
+          multiples.push('yu-gi-oh')
+        }
+        else {
+          multiples.push(i)
+        }
+
+      }
+      console.log(multiples);
+  }
+
+   checkYuGiOh("fizzbuzz is meh");
---
+++ convert.js
@@ -0,0 +1,25 @@
+/*
+Program: Zuri I4G Internship Training
+Exercise: FrontEnd Task 4A
+Intern Name: Solomon Ibeh;
+Slack Username: Solomon;
+Repo URL: 
+*/
+
+function convertFahrToCelcius(Fahr) {
+    if (Fahr == null) {
+    	console.log ('Kindly specify an actual number')
+    }
+    else if (typeof Fahr !== "string" && typeof Fahr !== "number" && !Array.isArray(Fahr)) {
+        console.log(`${Fahr} is not a valid number but a/an ${typeof Fahr}.`);
+    } else if (Array.isArray(Fahr)) {
+        console.log(`${Fahr} is not a valid number but a/an array.`);
+    } else {
+        let Celsius = ((Fahr-32)*5)/9;
+        console.log(Celsius.toFixed(4));
+    }
+}
+
+convertFahrToCelcius(0);
+
+
