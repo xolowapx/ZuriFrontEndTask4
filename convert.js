9111135712eeb5c02d9c06c1b12d7e53195f01db
xolowapx <53216282+xolowapx@users.noreply.github.com>
2021-05-17 15:25:01 +0100

    Update convert.js

--- convert.js
+++ convert.js
@@ -3,7 +3,7 @@ Program: Zuri I4G Internship Training
 Exercise: FrontEnd Task 4A
 Intern Name: Solomon Ibeh;
 Slack Username: Solomon;
-Repo URL: 
+Repo URL: https://xolowapx.github.io/ZuriFrontEndTask4/
 */
 
 function convertFahrToCelcius(Fahr) {
